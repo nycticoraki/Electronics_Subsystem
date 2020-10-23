e38eda7f1544dd1cd649bada8f546aff1f0cab35
Nycticoraci <37623397+Nycticoraci@users.noreply.github.com>
2020-10-22 20:09:28 -0400

    imu file updates

--- test/imu_test.py
+++ test/imu_test.py
@@ -5,6 +5,9 @@ import busio
 import adafruit_bno055
 i2c = busio.I2C(board.SCL, board.SDA)
 sensor = adafruit_bno055.BNO055_I2C(i2c)
+
+fo = open("imu_output.txt", "wb")
+
 print('Temperature: {} degrees C'.format(sensor.temperature))
 print('Accelerometer: (m/s^2): {}'.format(sensor.acceleration))
 print('Magnetometer (microteslas): {}'.format(sensor.magnetic))
@@ -13,3 +16,5 @@ print('Euler angle: {}'.format(sensor.euler))
 print('Quaternion: {}'.format(sensor.quaternion))
 print('Linear acceleration (m/s^2): {}'.format(sensor.linear_acceleration))
 print('Gravity (m/s^2): {}'.format(sensor.gravity))
+
+fo.close()
